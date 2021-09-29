9696e3048107c107ca130bb0959e319cfeb54cc5
Akhilesh235 <90238925+Akhilesh235@users.noreply.github.com>
2021-09-29 13:34:30 +0800

    Add project files.

---
+++ Wednesday Personal 2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wednesday_Personal_2
+{
+    public delegate void SampleDelegate();
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SampleDelegate del = new SampleDelegate(SampleMethod1);
+            del();
+
+        }
+
+        public static void SampleMethod1()
+        {
+            Console.WriteLine("Method 1 invoked");
+            Console.ReadLine();
+        }
+    }
+
+    
+}
